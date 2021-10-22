e2cb6127636a29e2db4c3d9975a7aca29f373f32
Ricardo Sardón <rst.bcnb@gmail.com>
2021-10-22 22:01:29 +0200

    auth0/completed

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -2,20 +2,19 @@ import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export const Profile = () => {
+
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   useEffect(()=> {
     if(isAuthenticated){
       localStorage.setItem("email", user.email);
       localStorage.setItem("isLogged", true);
-
     }
   }, [user.email]);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
-
 
   return (
     isAuthenticated && (
@@ -25,3 +24,4 @@ export const Profile = () => {
     )
   );
 }; 
+
