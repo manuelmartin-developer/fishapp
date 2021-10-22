71eac820c18878e9be8f0d5727a0bb2a4d2726a9
Manuel Martin <manuelmad@homeria.com>
2021-10-22 23:04:39 +0200

    code: finish endpoints api

--- server/models/sql_detailFish_model.js
+++ server/models/sql_detailFish_model.js
@@ -1,45 +1,39 @@
 const pool = require('../utils/dbpg');
 
-const fishes = {
+const getDetailsFromDB = async (name) => {
 
-    // RECUPERA TODOS LOS PECES DE LA DB
-    getFishes: async () => {
+    let client, result;
+    try {
+        client = await pool.connect();
+        result = await client.query('SELECT * FROM peces WHERE nombre=$1', [name]);
 
-        let client, result;
-        try {
-            client = await pool.connect();
-            result = await client.query(`
-            SELECT * FROM fishes
-            `)
-            // console.log(result);
-            return result
-        } catch (err) {
-            console.log(err);
-        } finally {
-            client.release();
-        }
-    },
-
-    getOnlyOneFish: async (info) => {
-
-        let client, result;
-        try {
-            //Connect
-            client = await pool.connect();
-
-            const result =  await client.query(`SELECT * FROM fishes 
-            WHERE info = $1
-            `,[info])
-
-        return result.rows[0] 
-
-
-        } catch (err) {
-            console.log(err);
-        } finally {
-            client.release();
-        }
-    },
+        return result.rows;
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
 }
 
-module.exports = fishes
+const getDiagnosisFromDB = async (name) => {
+
+    let client, result;
+    try {
+
+        client = await pool.connect();
+        result = await client.query(`SELECT * FROM enfermedades WHERE latin = $1 `, [name]);
+
+        return result.rows;
+
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.release();
+    }
+}
+
+
+module.exports = {
+    getDetailsFromDB,
+    getDiagnosisFromDB
+};
